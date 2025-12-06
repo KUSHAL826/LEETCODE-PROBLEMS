7225a0b24b1bbb13e22736c7b52396c1c96cf578
Kushal <kushalyngowda@gmail.com>
2025-12-06 14:57:12 +0530

    L19_Remove_Nth_Node_From_End

---
+++ L19_Remove_Nth_Node_from_end.c
@@ -0,0 +1,56 @@
+/*19. Remove Nth Node From End of List
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Example 1:
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) 
+{
+    struct ListNode *temp=head,*prev;
+    int i,pos,s=0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        s++;
+    }
+    pos=s-n;
+    if(pos==0)
+    {
+        temp=head;
+        head=head->next;
+        free(temp);
+        return head;
+    }
+    prev=NULL;
+    temp=head;
+    for(i=0;i<pos && temp!=NULL;i++)
+    {
+        prev=temp;
+        temp=temp->next;
+    }
+    if(temp==NULL)
+    {
+        return head;
+    }
+    else
+    {
+        prev->next=temp->next;
+        free(temp);
+        return head;
+    }
+}
